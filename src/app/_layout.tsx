ee06c595b64207f1d3cb032cdcf688b891483979
iltonkp <iltonk.si@gmail.com>
2024-03-24 14:37:22 +0000

    feat: cofiguring font roboto

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,25 @@
 import '@/styles/global.css'
 
-import { StatusBar } from 'react-native'
+import { StatusBar, View } from 'react-native'
 import { Slot } from 'expo-router'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function Layout(): React.ReactElement {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
   return (
-    <>
+    <View className="flex-1">
       <StatusBar barStyle="light-content" />
-      <Slot />
-    </>
+      {fontsLoaded === true && <Slot />}
+    </View>
   )
 }
